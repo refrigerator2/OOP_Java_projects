94868f43f0c529620e0a1c51398196a294241eae
refrigerator2 <ti.mak4710@gmail.com>
2026-05-19 11:23:33 +0200

    Continue with developing moving logic

--- Checkers/Board.java
+++ Checkers/Board.java
@@ -96,7 +96,7 @@ public class Board {
       int targetY = chY + dirY[i];
 
       if (pointInBorders(targetX, targetY)) {
-        if (helperForNextFunction(ch, targetX, targetY)) {
+        if (helperForNextFunction(ch, targetX, targetY) && checkIfBeatable(ch, targetX, targetY)) {
           return true;
         }
       }
@@ -104,15 +104,19 @@ public class Board {
     return false;
   }
 
-  public boolean moveChecker(Checker ch, int toX, int toY) {
-    if (!pointInBorders(toX, toY)) {
-      System.out.println("Coordinates are out of bounds!");
+  boolean checkIfCheckerMovesToAnotherColorChecker(Checker ch, int x, int y) {
+    Tile t = tiles[y * SIZE + x];
+    if (!t.getIsBlack())
       return false;
-    }
-    if (checkIfCheckerOfDifferentColorInTheArea(ch)) {
-      System.out.println("Here is a different color checker in the area");
+    Checker check = t.getCheck();
+    if (check == null)
       return false;
-    }
+    if (check.getIsBlack() != ch.getIsBlack())
+      return true;
+    return false;
+  }
+
+  boolean moveCheckerToTile(Checker ch, int toX, int toY) {
     int chX = ch.getPosX();
     int chY = ch.getPosY();
     Tile bef = tiles[chY * SIZE + chX];
@@ -124,6 +128,54 @@ public class Board {
       ch.setPosY(toY);
     } else {
       System.out.println("Tile is white");
+    }
+    return true;
+  }
+
+  boolean checkIfBeatable(Checker ch, int toX, int toY) {
+    int chX = ch.getPosX();
+    int chY = ch.getPosY();
+
+    int difX = toX - chX;
+    int difY = toY - chY;
+
+    if (toX + difX < 0 || toX + difX >= SIZE || toY + difY < 0 || toY + difY >= SIZE)
+      return false;
+    Tile t = tiles[(toY + difY) * SIZE + (toX + difX)];
+    if (t.getCheck() != null) {
+      return false;
+    }
+    return true;
+  }
+
+  void beatChecker(int toX, int toY) {
+    Tile t = tiles[toY * SIZE + toX];
+    Checker c = t.getCheck();
+    c = null;
+    t.setCheck(null);
+  }
+
+  public boolean moveChecker(Checker ch, int toX, int toY) {
+    if (!pointInBorders(toX, toY)) {
+      System.out.println("Coordinates are out of bounds!");
+      return false;
+    }
+    boolean goesToDifColor = checkIfCheckerMovesToAnotherColorChecker(ch, toX, toY);
+    if (!goesToDifColor) {
+      if (checkIfCheckerOfDifferentColorInTheArea(ch)) {
+        System.out.println("Here is a different color checker in the area");
+        return false;
+      }
+      return moveCheckerToTile(ch, toX, toY);
+    } else {
+      boolean beatable = checkIfBeatable(ch, toX, toY);
+      if (beatable) {
+        beatChecker(toX, toY);
+        moveCheckerToTile(ch, toX + (toX - ch.getPosX()), toY + (toY - ch.getPosY()));
+      } else {
+        System.out.println("You cannot move here because checker is secured");
+        return false;
+      }
     }
     return true;
   }
